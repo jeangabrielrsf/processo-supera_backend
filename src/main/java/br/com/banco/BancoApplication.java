42fab25f8b4cac3631b75761f4b9443b2f14690d
Jean Gabriel Frusca <jeang96@gmail.com>
2023-07-18 20:46:24 -0300

    feat: enable CORS

--- src/main/java/br/com/banco/BancoApplication.java
+++ src/main/java/br/com/banco/BancoApplication.java
@@ -2,12 +2,25 @@ package br.com.banco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BancoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BancoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/transferencias").allowedOrigins("http://localhost:8080");
+            }
+        };
     }
 
 }
--- src/main/java/br/com/banco/controllers/TransactionsController.java
+++ src/main/java/br/com/banco/controllers/TransactionsController.java
@@ -4,6 +4,7 @@ import java.security.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,25 +15,30 @@ import br.com.banco.services.TransactionService;
 
 @RestController
 @RequestMapping
+
 public class TransactionsController {
     @Autowired
     private TransactionService transactionService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/transferencias")
     public List<Transaction> listAllTransactions() {
         return transactionService.listAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/transferencias/conta/{accountId}")
     public List<Transaction> listAccountTransactions(@PathVariable int accountId) {
         return transactionService.listByAccountId(accountId);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/transferencias/operador/{nomeOperador}")
     public List<Transaction> listOperadorTransactions(@PathVariable String nomeOperador) {
         return transactionService.listByName(nomeOperador);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/transferencias/{initialDate}-{finalDate}") 
         public List<Transaction> listDatesTransaction(@PathVariable java.sql.Timestamp initialDate, java.sql.Timestamp finalDate) {
             return transactionService.listByDateRange(initialDate, finalDate);
